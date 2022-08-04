fec5a0856ea47d1ba45ea665798be73965d2b7b8
LAPTOP-1J63I905\Owner <mineralkim@skuniv.ac.kr>
2022-08-04 09:42:38 +0900

    review reviewlike 수정본

--- miniWorkSpace/miniPrj01/src/trip/kms/lodgingReview/LodgingReviewController.java
+++ miniWorkSpace/miniPrj01/src/trip/kms/lodgingReview/LodgingReviewController.java
@@ -2,6 +2,11 @@ package trip.kms.lodgingReview;
 
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Scanner;
+
+import trip.kms.lodgingReviewLike.LodgingReviewLikeController;
+import trip.kms.lodgingReviewLike.LodgingReviewLikeVo;
+import trip.min.member.MemberVo;
 
 public class LodgingReviewController {
 
@@ -29,5 +34,77 @@ public class LodgingReviewController {
 			
 		}
 		
+		LodgingReviewVo vo = new LodgingReviewVo();
+		while(true) {
+			System.out.println("==== 리뷰 번호 선택 ====");
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("조회할 리뷰 번호  :");
+			String inputNumStr = scanner.nextLine();
+			int reviewNum = Integer.valueOf(inputNumStr);
+			
+			for(int i = 0 ; i < lodgingReviewVoList.size() ; i++) {
+				LodgingReviewVo tmp = lodgingReviewVoList.get(i);
+				if(tmp.getNo() == reviewNum) {
+					int no = tmp.getNo(); 
+					int writer = tmp.getWriter(); 
+					int lodgingNo = tmp.getLodgingNo(); 
+					String title = tmp.getTitle(); 
+					String content = tmp.getContent(); 
+					Timestamp reviewDate = tmp.getReviewDate(); 
+					Timestamp editDate = tmp.getEditDate(); 
+					int reviewLike = tmp.getReviewLike();
+					
+					vo.setNo(no);
+					vo.setWriter(writer);
+					vo.setLodgingNo(lodgingNo);
+					vo.setTitle(title);
+					vo.setContent(content);
+					vo.setReviewDate(reviewDate);
+					vo.setEditDate(editDate);
+					vo.setReviewLike(reviewLike);
+					new LodgingReviewLikeController().showReviewLike(vo);
+					break;
+				} else {
+					System.out.println("존재하지 않는 리뷰 번호입니다.");
+				}
+			}
+		}
+		
+	}
+	
+	//lodgingreviewlikecontroller에서 게시글 수정 선택시
+	public void editReview(LodgingReviewVo vo) {
+		System.out.println("===== 게시글 수정 =====");
+		Scanner scanner = new Scanner(System.in);
+		
+		//membervo 받아와서 pwd 검사?
+		
+		System.out.print("수정할 title : ");
+		String title = scanner.nextLine();
+		System.out.print("수정할 content : ");
+		String content = scanner.nextLine();
+		
+		LodgingReviewVo lodgingReviewVoEdit = new LodgingReviewVo();
+		lodgingReviewVoEdit.setTitle(title);
+		lodgingReviewVoEdit.setContent(content);
+		lodgingReviewVoEdit.setLodgingNo(vo.getNo());
+		lodgingReviewVoEdit.setWriter(vo.getWriter());
+		
+		int result = new LodgingReviewService().editReview(lodgingReviewVoEdit);
+		if(result == 1) {
+			System.out.println("수정 완료");
+		} else {
+			System.out.println("수정 실패");
+		}
+		
+		
+	}
+	//lodgingreviewlikecontroller에서 게시글 삭제 선택시
+	public void deleteReview() {
+		System.out.println("===== 게시글 삭제 =====");
+		LodgingReviewVo lodgingReviewVoDelete = new LodgingReviewVo();
+//		MemberVo memberVo = new MemberVo();
+		
+		new LodgingReviewService().editReview(lodgingReviewVoDelete);
 	}
 }
--- miniWorkSpace/miniPrj01/src/trip/kms/lodgingReview/LodgingReviewService.java
+++ miniWorkSpace/miniPrj01/src/trip/kms/lodgingReview/LodgingReviewService.java
@@ -5,38 +5,73 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 
+import trip.min.common.JDBCTemplate;
+import trip.min.main.MemberMain;
+
 public class LodgingReviewService {
 
 	public List<LodgingReviewVo> showList() {
-		
 		Connection conn = null;
 		List<LodgingReviewVo> lodgingReviewVoList = null;
 		
+		if(MemberMain.LoginMember == null) {
+			System.out.println("로그인을 먼저 진행해주세요.");
+			return lodgingReviewVoList; //다음 내용 진행 막기(빈껍대기)
+		}
+		
 		try {
 			//connect db
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-			String id = "C##ATT";
-			String pwd = "ATT";
-			
-			conn = DriverManager.getConnection(url, id, pwd);
-			System.out.println("Connection : " + conn);
-			conn.setAutoCommit(false);
+			conn = JDBCTemplate.getConnection();
 			
 			lodgingReviewVoList = new LodgingReviewDao().showList(conn);
+			JDBCTemplate.commit(conn);
+			
+		} catch (Exception e) {
+			JDBCTemplate.rollBack(conn);
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(conn);
+		}
+		
+		return lodgingReviewVoList;
+	}
+	
+	public int editReview(LodgingReviewVo lodgingReviewVoEdit) {
+		Connection conn = null;
+		int result = 0;
+		try {
+			conn = JDBCTemplate.getConnection();
+			result = new LodgingReviewDao().editReview(conn, lodgingReviewVoEdit);
+			
+			if(result == 1) {
+				JDBCTemplate.commit(conn);
+			} else {
+				JDBCTemplate.rollBack(conn);
+			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			try {
-				if(conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			JDBCTemplate.close(conn);
 		}
 		
-		return lodgingReviewVoList;
+		return result;
+		
+	}
+	
+	public void deleteReview(LodgingReviewVo lodgingReviewVoDelete) {
+		Connection conn = null;
+		
+		try {
+			conn = JDBCTemplate.getConnection();
+			new LodgingReviewDao().deleteReview(conn);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(conn);
+		}
+		
 	}
 
 }
